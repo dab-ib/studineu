fe6ca197d38097ad3c4abd751a5555c677bfdf1f
dab-ib <77733201+dab-ib@users.noreply.github.com>
2023-02-21 19:07:25 +0100

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Kameras</h1>
+    <ul>
+      <li v-for="camera in cameras" :key="camera.id">
+        {{ camera.name }} - {{ camera.url }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      cameras: []
+    }
+  },
+  mounted() {
+    this.loadCameras()
+  },
+  methods: {
+    loadCameras() {
+      axios.get('/api/cameras')
+        .then(response => {
+          this.cameras = response.data.cameras
+        })
+    }
+  }
+}
+</script>
