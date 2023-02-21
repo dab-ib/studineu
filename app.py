92293db617be3cf75739e092de4264147e87e923
dab-ib <77733201+dab-ib@users.noreply.github.com>
2023-02-21 19:08:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,56 @@
 from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# Konfiguration für MariaDB
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'nvr_system'
+
+mysql = MySQL(app)
 
 # Endpunkt zur Anzeige aller Kameras in der Datenbank
 @app.route('/api/cameras', methods=['GET'])
 def get_cameras():
-    # Hier wird Code für die Abfrage aller Kameras aus der Datenbank ausgeführt
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM cameras")
+    rows = cur.fetchall()
+    cur.close()
+
+    cameras = []
+    for row in rows:
+        cameras.append({'id': row[0], 'name': row[1], 'url': row[2]})
     return jsonify({'cameras': cameras})
 
 # Endpunkt zum Hinzufügen einer neuen Kamera
 @app.route('/api/cameras', methods=['POST'])
 def add_camera():
-    # Hier wird Code für das Hinzufügen einer neuen Kamera zur Datenbank ausgeführt
+    name = request.json['name']
+    url = request.json['url']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO cameras (name, url) VALUES (%s, %s)", (name, url))
+    mysql.connection.commit()
+    cur.close()
     return jsonify({'success': True})
 
 # Endpunkt zur Aktualisierung einer vorhandenen Kamera
 @app.route('/api/cameras/<int:camera_id>', methods=['PUT'])
 def update_camera(camera_id):
-    # Hier wird Code für das Aktualisieren einer vorhandenen Kamera in der Datenbank ausgeführt
+    name = request.json['name']
+    url = request.json['url']
+    cur = mysql.connection.cursor()
+    cur.execute("UPDATE cameras SET name=%s, url=%s WHERE id=%s", (name, url, camera_id))
+    mysql.connection.commit()
+    cur.close()
     return jsonify({'success': True})
 
 # Endpunkt zum Löschen einer vorhandenen Kamera
 @app.route('/api/cameras/<int:camera_id>', methods=['DELETE'])
 def delete_camera(camera_id):
-    # Hier wird Code für das Löschen einer vorhandenen Kamera aus der Datenbank ausgeführt
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM cameras WHERE id=%s", (camera_id,))
+    mysql.connection.commit()
+    cur.close()
     return jsonify({'success': True})
-
-if __name__ == '__main__':
-    app.run(debug=True)
